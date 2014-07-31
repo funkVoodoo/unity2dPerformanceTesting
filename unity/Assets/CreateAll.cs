c83c348160bbe42a07ef218bd02ece579395f254
funkVoodoo <funkVoodoo@gmail.com>
2014-07-31 10:16:06 +0100

    Initial commit

---
+++ unity/Assets/CreateAll.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateAll : MonoBehaviour {
+	
+
+	public static int dynamicSpawnCount;
+	
+	public GameObject[]	 injectObject = null; 
+	public int	spawnCount = 10;
+	public float speed = 200f;
+
+	// Use this for initialization
+	void Start () {
+		
+		float currentPos = 0f;
+		dynamicSpawnCount = 0;
+		
+		if (injectObject != null) 
+		{
+			for (int a=0; a < spawnCount; a++)
+			{
+				//Chose a random posistion.
+				int num = Random.Range(0, injectObject.Length);
+
+				//Add to the world.. 
+				Vector3 newPos = new Vector3(currentPos,0,0);
+				Instantiate(injectObject[num], newPos, transform.rotation);
+				
+//				localObject = Instantiate(injectCrystalObject, mapObject.transform.position, mapObject.transform.rotation) as GameObject;
+//				localObject.transform.parent = mapObject.transform;
+								
+				currentPos+=5.0f;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		
+		Vector3 tmp = new Vector3(speed *Time.deltaTime,0,0);
+		transform.position += tmp;
+		
+		
+	}
+
+
+}
---
+++ unity/Assets/SpawnChildNodes.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnChildNodes : MonoBehaviour {
+
+	public GameObject crystal;
+	public GameObject mobOne;
+	public GameObject mobTwo;
+
+	private GameObject localCrystal;
+	private GameObject localMobOne;
+	private GameObject localMobTwo;
+
+	private Vector3 parentPosistion;
+	private Quaternion parentRotation;
+
+	void Start()
+	{
+		parentRotation = transform.rotation;
+		parentPosistion = transform.position;
+	}
+	
+	void OnBecameVisible()
+	{
+			//Add a crystal
+			localCrystal = Instantiate(crystal, parentPosistion, parentRotation) as GameObject;
+			localCrystal.transform.parent = transform;
+			CreateAll.dynamicSpawnCount++;
+			
+	}
+	
+	void OnBecameInvisible()
+	{
+		CreateAll.dynamicSpawnCount--;
+		Destroy (localCrystal);
+
+	}
+}
